0a0f57d26c8a42ff877066a1b0c06fee3671658c
otacosboulogne <217191891+otacosboulogne@users.noreply.github.com>
2026-09-06 21:41:54 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   // Colle ici l'URL /exec de ton déploiement Google Apps Script.
-  GAS_URL: 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE'
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbwOUEvtUGybO7z_3kz00BFcaEUQaiE_nsfoA9wBVqS13Ly9JViyMCZgK-YZiCN0wBUeuw/exec'
 };
